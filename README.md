b5e75386aa19463fc47c9298e530bafe772bb4aa
bvekp1@gmail.com <bvekp1@gmail.com>
2019-12-17 08:25:00 +0545

    complete work

---
+++ README.md
@@ -0,0 +1,10 @@
+1.For runing this api first create a directory and then clone the  repo  https://classroom.github.com/a/AYpdKz-Q using git clone command.
+2. Install the following package to run the application:
+    i.express
+    ii.multer
+    iii.mysql2
+    iv.nodemon
+    v. sequelize
+    vi. swagger-jsdoc
+    vii. swagger-ui-express
+    viii. eslint
--- index.js
+++ index.js
@@ -1,10 +1,89 @@
 var express = require('express');
-var bodyparser = require('body-parser');
 var imageController = require('./controller/imageController.js');
 var app = express();
 
+const swaggerjsdoc = require('swagger-jsdoc');
+
+const swaggerUIExpress = require('swagger-ui-express');
+
+var swaggerDefinition = {
+	info: {
+		title: 'Image Docs',
+		version: '0.0.1',
+		description: 'This is for image documentaion'
+	},
+	securityDefination: {
+			bearerAuth:{
+				type: 'api',
+				name: 'authorization',
+				scheme: 'bearer',
+				in: 'header'
+			}
+	},
+	host: 'localhost:3022',
+	basePath: '/'
+}
+
+
+var swaggerOptions = {
+	swaggerDefinition,
+	apis: ['./index.js']
+}
+
+var swaggerSpecs = swaggerjsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUIExpress.serve, swaggerUIExpress.setup(swaggerSpecs));
+/**
+* @swagger
+* /image:
+*  post:
+*   tags:
+*    - Image
+*   description: Testing
+*   produces:
+*    - application/json
+*   consumes:
+*    -form-data
+*   parameters:
+*    - name: image
+*      in: formData
+*      required: true
+*      type: file
+*      description: Select image
+*   responses:
+*    300:
+*     description: Image uploaded successfully
+*    500:
+*     description: Couldn't upload image
+*/
+
 app.post('/image', imageController.ImageName, imageController.uploadImage);
+
+/**
+* @swagger
+* /images:
+*  post:
+*   tags:
+*    - Images
+*   description: Testing
+*   produces:
+*    - application/json
+*   consumes:
+*    -form-data
+*   parameters:
+*    - name: images
+*      in: formData
+*      required: true
+*      type: file
+*      description: Select images
+*   responses:
+*    300:
+*     description: Image uploaded successfully
+*    500:
+*     description: Couldn't upload image
+*/
+
 app.post('/imageSave', imageController.uploadBibek, imageController.uploadMultiple);
 
 
-app.listen('3032');
+app.listen('3022');
